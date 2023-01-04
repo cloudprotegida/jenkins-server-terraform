740a411af14a3aa4eb901de061d045fa562f5f60
cloudprotegida <121353683+cloudprotegida@users.noreply.github.com>
2023-01-03 21:12:38 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,18 +15,17 @@ variable "tags" {
     Name    = "jenkins-manager"
     Product = "jenkins"
     Team    = "sre"
-    automate = "Auto-Shutdown"
   }
 }
 
 variable "cidr" {
   default = [
-    "189.36.195.211/32"
+    "0.0.0.0/32"
   ]
 }
 
 variable "subnet_id" {
-  default = "subnet-0115a35fc11b6e31d"
+  default = ""
 }
 
 variable "key_name" {
@@ -34,5 +33,5 @@ variable "key_name" {
 }
 
 variable "key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCsahoCKAmIyYY9ycUUJJibYokWTbiDd+Pcmkqdz3Zv8hs4+4++sZRs0nuTM7ViLGnyqz6yu00+k3UmGCt29xeYPm7vqch1+QSokvEXP5z3zRiO2KLm9x8ZyDUcIKYGLfurLhX50Hftyj+H/h8YMiQ1/rKHLxDtvaCuDhydjJoEsKqRzppcxsy4HcLyppHSL48iJispObTbmfmIg4zlYA+7wB+091ghOQlYLdHxeAat/2mjQRpgN9Msa8HF5mPM1gxgXXwpFv0Xoa5I57HbXObAaSHm2O7aroXIEVf2gsBrIhRx7y0dTkgrHM6lW3geX8lRW2KBel1zh+5Onkl/Z+WnKowUl349AH/CrozsG8LAR0dN7ubAVYmtRaLZYdKDgYaRsVmcPOXPnlN/daqHztyI61Za8LH2g7AcxWtNCpWs1PTEo5V1AB3AcvSLUjaDm/Ek1Ak0HiGAKwX3ZCEYUc9vmCIQr5UadQ9CkeIIybxjSfBBJAMOtpB59u7dcnJA6DU="
+  default = ""
 }
